01627c41857ccecd44b8dd13512b3bc396d9b6c3
Steven-Ou <osteve425@gmail.com>
2025-10-21 00:57:52 -0400

    completing the insertion sort function

--- Insertion.java
+++ Insertion.java
@@ -1,7 +1,13 @@
-public class Insertion{
-    public static void insertion_sort(int[] a){
-        for(int i =0; i<a.length; i++){
-            int k=a[i];
+public class Insertion {
+    public static void insertion_sort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int k = a[i];
+            int j = i-1; 
+            while(i>0 && a[i]>k){
+                a[i+1]=a[i];
+                i=i+1;
+            }
+            a[i+1] = k; 
         }
     }
 }
