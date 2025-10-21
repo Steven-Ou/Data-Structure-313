4be521f541a0317a96136189452dd8afdbae9d34
Steven-Ou <osteve425@gmail.com>
2025-10-21 00:59:50 -0400

    fixing

--- Insertion.java
+++ Insertion.java
@@ -1,13 +1,13 @@
 public class Insertion {
     public static void insertion_sort(int[] a) {
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 2; i < a.length; i++) {
             int k = a[i];
             int j = i-1; 
-            while(i>0 && a[i]>k){
-                a[i+1]=a[i];
-                i=i+1;
+            while(j>0 && a[j]>k){
+                a[j+1]=a[j];
+                j=j+1;
             }
-            a[i+1] = k; 
+            a[j+1] = k; 
         }
     }
 }
