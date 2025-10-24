9975dc78fea0c82080c08a7e0ea44dade6c8b8a6
Steven-Ou <osteve425@gmail.com>
2025-10-23 20:53:21 -0400

    creating the temporary arrays and all the copying the data with a loop

--- Sort.java
+++ Sort.java
@@ -19,6 +19,22 @@ public class Sort {
         return -1;
     }
     public static void merge(int arr[], int p, int q, int r){
-    
+        int n1 = q-p +1;
+        int n2 = r-q;
+
+        int L[n1];
+        int R[n2];
+
+        for(int i=0;i<n1;i++){
+            L[i]=arr[p+1];
+        }
+        for(int j=0;j<n2;j++){
+            R[j]=arr[q+1+j];
+        }
+        int i =0;
+        int j=0;
+        int k=p;
+
+        
     }
 }
