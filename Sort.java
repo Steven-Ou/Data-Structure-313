61c5d800a63e33f7c7b9193495ea33a07fec0ae9
Steven-Ou <osteve425@gmail.com>
2025-10-28 20:54:23 -0400

    looping through the first and last nodes, and then swapping it around base off the amount number that was put in, and if its less then x.

--- Sort.java
+++ Sort.java
@@ -85,5 +85,12 @@ public class Sort {
     public static int partition(int arr[], int p, int r){
         int x = arr[r];
         int i = p-1;
+
+        for(int j=p;j<r;j++){
+            if(arr[j]<=x){
+                i++;
+                swap(arr[i],arr[j]);
+            }
+        }
     }
 }
