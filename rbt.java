bdc8a56e3f39dd2f598b6e69ad2eb740b8aa92dc
Steven-Ou <osteve425@gmail.com>
2025-10-30 01:45:10 -0400

    Initializing the variable inside the node class

--- rbt.java
+++ rbt.java
@@ -2,5 +2,13 @@ public class rbt {
     enum Color{
         red, black
     }
-    
+    class Node{
+        int key;
+        Color color;
+        Node p;
+        Node left;
+        Node right; 
+
+        
+    }
 }
