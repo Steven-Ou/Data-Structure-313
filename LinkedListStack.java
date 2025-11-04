28e494ad62310bf7a8d2933278cd7c87e296e835
Steven-Ou <osteve425@gmail.com>
2025-11-03 20:48:46 -0500

    creating the blueprint

--- LinkedListStack.java
+++ LinkedListStack.java
@@ -1,7 +1,14 @@
+import java.lang.classfile.components.ClassPrinter.Node;
 import java.util.EmptyStackException;
 
 class Node<T>{
-    
+    T data;
+    Node<T> next;
+
+    public Node(T data){
+        this.data = data;
+        this.next =null;
+    }
 }
 public class LinkedListStack {
     
