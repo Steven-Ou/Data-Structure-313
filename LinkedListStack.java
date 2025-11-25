220a5a43382a76456b11b25bec66b473add5646c
Steven-Ou <osteve425@gmail.com>
2025-11-25 00:05:09 -0500

    formatting

--- LinkedListStack.java
+++ LinkedListStack.java
@@ -1,69 +1,76 @@
 import java.util.EmptyStackException;
 
-class Node<T>{
+class Node<T> {
     T data;
     Node<T> next;
 
-    public Node(T data){
+    public Node(T data) {
         this.data = data;
-        this.next =null;
+        this.next = null;
     }
 }
+
 public class LinkedListStack<T> {
-    private static class B{
+    private static class B {
         int key;
         B left;
         B right;
         B parent;
-        public B(int key){
-            this.key=key;
+
+        public B(int key) {
+            this.key = key;
             this.left = null;
             this.right = null;
             this.parent = null;
         }
     }
+
     private Node<T> top;
     private Node<T> head;
-    public LinkedListStack(){
-        this.top =null;
+
+    public LinkedListStack() {
+        this.top = null;
     }
 
-    public boolean isEmpty(){
-        return top ==null;
+    public boolean isEmpty() {
+        return top == null;
     }
 
-    public void push(T value){
+    public void push(T value) {
         Node<T> newNode = new Node<>(value);
 
-        newNode.next=top;
+        newNode.next = top;
         top = newNode;
         System.out.println("Pushed:" + value);
     }
 
-    public T pop(){
-        if(isEmpty()){
+    public T pop() {
+        if (isEmpty()) {
             throw new EmptyStackException();
         }
 
-        T dataToReturn = top.data; //storing the data to return
+        T dataToReturn = top.data; // storing the data to return
         top = top.next; // Update top to point to the next node
 
-        System.out.println("Popped: "+ dataToReturn);
+        System.out.println("Popped: " + dataToReturn);
         return dataToReturn;
     }
-    public T peek(){
-        if (isEmpty()){
+
+    public T peek() {
+        if (isEmpty()) {
             throw new EmptyStackException();
         }
         return top.data;
     }
-    public void insertAtHead(T value){
+
+    public void insertAtHead(T value) {
         Node<T> newNode = new Node<>(value);
         newNode.next = this.head;
-        this.head= newNode;
+        this.head = newNode;
     }
-    public T deleteHead(){
-        if(isEmpty()){
+
+    public T deleteHead() {
+        if (isEmpty()) {
             return null;
         }
         T data = head.data;
@@ -71,21 +78,22 @@ public class LinkedListStack<T> {
 
         return data;
     }
-    public T deleteTail(){
-        if(isEmpty()){
+
+    public T deleteTail() {
+        if (isEmpty()) {
             return null;
         }
-        if(head.next == null){
+        if (head.next == null) {
             T data = head.data;
             head = null;
-            return data; 
+            return data;
         }
 
-        Node<T> current =head;
-        Node<T> previous =null;
+        Node<T> current = head;
+        Node<T> previous = null;
 
-        while(current.next != null){
-            previous = current ;
+        while (current.next != null) {
+            previous = current;
             current = current.next;
         }
 
@@ -94,67 +102,73 @@ public class LinkedListStack<T> {
 
         return data;
     }
-    public boolean deleteByValue(T value){
-        if(isEmpty()){
+
+    public boolean deleteByValue(T value) {
+        if (isEmpty()) {
             return false;
         }
-        if (head.data.equals(value)){
+        if (head.data.equals(value)) {
             head = head.next;
             return true;
         }
 
-        Node<T> current= head;
+        Node<T> current = head;
         Node<T> previous = null;
 
-        while((current !=null && !current.data.equals(value))){
-            previous =current;
+        while ((current != null && !current.data.equals(value))) {
+            previous = current;
             current = current.next;
         }
-        
-        if(current ==null){
+
+        if (current == null) {
             return false;
         }
         previous.next = current.next;
         return true;
     }
-    public static B treeMinimumRecursive(B x){
-        if(x ==null){
+
+    public static B treeMinimumRecursive(B x) {
+        if (x == null) {
             return null;
         }
-        if(x.left ==null){
+        if (x.left == null) {
             return x;
         }
         return treeMinimumRecursive(x.left);
     }
-    public static B treeMaximumRecursive(B x){
-        if(x==null){
+
+    public static B treeMaximumRecursive(B x) {
+        if (x == null) {
             return null;
         }
-        if(x.right==null){
+        if (x.right == null) {
             return x;
         }
         return treeMaximumRecursive(x.right);
     }
-    public static B treePredecessor(B x){
-        if(x==null){
+
+    public static B treePredecessor(B x) {
+        if (x == null) {
             return null;
         }
-        if(x.left !=null){
+        if (x.left != null) {
             return treeMaximumRecursive(x.left);
         }
 
         B y = x.parent;
-        B current =x;
+        B current = x;
 
-        while(y!= null &&current ==y.left){
-            current =y;
-            y=y.parent;
+        while (y != null && current == y.left) {
+            current = y;
+            y = y.parent;
         }
         return y;
     }
-    public static B insertRecursive(B root, int key){
-        return insertRecHelper(root,null,key);
+
+    public static B insertRecursive(B root, int key) {
+        return insertRecHelper(root, null, key);
     }
+
     private static B insertRecHelper(B current, B p, int key) {
         if (current == null) {
             B newNode = new B(key);
@@ -168,49 +182,56 @@ public class LinkedListStack<T> {
         }
         return current;
     }
-    public static void inorder(B root){
-        if( root!=null){
+
+    public static void inorder(B root) {
+        if (root != null) {
             inorder(root.left);
-            System.out.print(root.key+" ");
+            System.out.print(root.key + " ");
             inorder(root.right);
         }
     }
+
     // a. Count total nodes
     public static int countNodes(B t) {
-        if (t == null) return 0;
+        if (t == null)
+            return 0;
         return 1 + countNodes(t.left) + countNodes(t.right);
     }
-    //Counting the leaves
+
+    // Counting the leaves
     public static int countLeaves(B t) {
-        if (t == null) return 0;
-        if (t.left == null && t.right == null) return 1;
+        if (t == null)
+            return 0;
+        if (t.left == null && t.right == null)
+            return 1;
         return countLeaves(t.left) + countLeaves(t.right);
     }
-    //Count full nodes
+
+    // Count full nodes
     public static int countFullNodes(B t) {
-        if (t == null) return 0;
+        if (t == null)
+            return 0;
         int isFull = (t.left != null && t.right != null) ? 1 : 0;
         return isFull + countFullNodes(t.left) + countFullNodes(t.right);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         LinkedListStack<Integer> stack = new LinkedListStack<>();
 
         stack.push(10);
         stack.push(20);
         stack.push(30);
 
-        System.out.println("Current top (peek): "+ stack.peek());
+        System.out.println("Current top (peek): " + stack.peek());
 
-        try{
+        try {
             stack.pop();
             stack.pop();
-            System.out.println("Current top (peek): "+ stack.peek());
+            System.out.println("Current top (peek): " + stack.peek());
             stack.pop();
             stack.pop();
-        }catch(EmptyStackException e){
+        } catch (EmptyStackException e) {
             System.out.println("Error: Stack is empty!");
         }
     }
 }
-
