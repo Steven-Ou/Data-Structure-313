ff55c1f1e5fb150687afd3638d84b337eb9446e6
Steven-Ou <osteve425@gmail.com>
2025-12-06 01:27:40 -0500

    adding in the data structure classes for generations

--- Visual_Project/DSAexamprep.js
+++ Visual_Project/DSAexamprep.js
@@ -1,2 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { BookOpen, Code, RefreshCw, CheckCircle, XCircle, Activity, GitBranch, Database, Layers, Lightbulb, HelpCircle, ArrowRight } from 'lucide-react';
+
+class TreeNode {
+  constructor(val, color = "white") {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+    this.color = color; // For RBT
+    this.height = 1;    // For AVL
+    this.id = Math.random().toString(36).substr(2, 9);
+  }
+}
