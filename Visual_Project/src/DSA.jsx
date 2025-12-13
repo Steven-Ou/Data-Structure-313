d6463fc9e7b6ee39b57c2258d757ccb66a4f3b4c
Steven-Ou <osteve425@gmail.com>
2025-12-13 00:01:19 -0500

    organizing

--- Visual_Project/src/DSA.jsx
+++ Visual_Project/src/DSA.jsx
@@ -597,10 +597,10 @@ Return T`,
     },
     question: "Perform first pass (insert 2nd element).",
     code: `Insertion-Sort(A)
-  for j = 2 to A.length
-      key = A[j]; i = j - 1
-      while i > 0 and A[i] > key
-          A[i+1] = A[i]
+      for j = 2 to A.length
+        key = A[j]; i = j - 1
+          while i > 0 and A[i] > key
+        A[i+1] = A[i]
           i = i - 1
       A[i+1] = key`,
   },
