8529aab5fbbeb5196ada7572324a59b743b38577
Steven-Ou <osteve425@gmail.com>
2025-12-31 11:24:53 -0500

    adding in python

--- Visual_Project/src/DSA.jsx
+++ Visual_Project/src/DSA.jsx
@@ -683,6 +683,19 @@ void mergeSort(int arr[], int l, int r) {
     }
 }`
       ,
+      python:`def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L, R = arr[:mid], arr[mid:]
+        merge_sort(L); merge_sort(R)
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]: arr[k] = L[i]; i += 1
+            else: arr[k] = R[j]; j += 1
+            k += 1
+        while i < len(L): arr[k] = L[i]; i += 1; k += 1
+        while j < len(R): arr[k] = R[j]; j += 1; k += 1`,
+
       pseudo: `MERGE-SORT(A, p, r)
     if p < r then
         q = floor((p + r) / 2)
