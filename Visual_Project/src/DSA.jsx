09e6a5c61500c783de36c88966fe77c61cc80530
Steven-Ou <osteve425@gmail.com>
2026-01-18 01:10:46 -0500

    adding in the cpp bubbleSort version

--- Visual_Project/src/DSA.jsx
+++ Visual_Project/src/DSA.jsx
@@ -725,6 +725,11 @@ void mergeSort(int arr[], int l, int r) {
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
+}`,cpp: `void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
 }`,
     },
   },
