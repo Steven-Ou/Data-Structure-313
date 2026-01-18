83746e96fd9711b46e33ea59980cb0feb4ae2bf1
Steven-Ou <osteve425@gmail.com>
2026-01-17 22:07:35 -0500

    addding in java bubble sort

--- Visual_Project/src/DSA.jsx
+++ Visual_Project/src/DSA.jsx
@@ -716,7 +716,16 @@ void mergeSort(int arr[], int l, int r) {
         : "Loading...",
     hint: "Compare adjacent elements (arr[j], arr[j+1]) and swap if the first is greater than the second.",
     codes: {
-
+      java: `void bubbleSort(int[] arr) {
+    int n = arr.length;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+}`,
     },
   },
   quick_sort: {
