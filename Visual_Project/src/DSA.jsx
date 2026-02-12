f6b6f66aa5341de116c117531bacbc3ab4776429
Steven-Ou <osteve425@gmail.com>
2026-02-12 00:25:16 -0500

    adding in treenode

--- Visual_Project/src/DSA.jsx
+++ Visual_Project/src/DSA.jsx
@@ -15,16 +15,6 @@ import {
 
 // --- DATA STRUCTURE CLASSES ---
 
-class TreeNode {
-  constructor(val, color = "red") {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-    this.parent = null;
-    this.color = color;
-    this.id = Math.random().toString(36).substr(2, 9);
-  }
-}
 
 // --- HELPER FUNCTIONS ---
 
---
+++ Visual_Project/src/models/TreeNode.js
@@ -0,0 +1,10 @@
+class TreeNode {
+  constructor(val, color = "red") {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+    this.color = color;
+    this.id = Math.random().toString(36).substr(2, 9);
+  }
+}
